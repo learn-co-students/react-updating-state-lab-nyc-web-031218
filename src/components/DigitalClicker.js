c2d8ceb6a19301434b640aff8ae55dc405fecfc7
Dwire <gjdwyer8@gmail.com>
2018-05-18 00:09:02 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,27 @@
 // Code DigitalClicker Component Here
+import React, {Component} from 'react'
+
+class DigitalClicker extends Component {
+  state = {
+    timesClicked: 0
+  }
+
+  incrementor = () => {
+    const newCount = this.state.timesClicked += 1
+
+    this.setState({
+      timesClicked: newCount
+    })
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Clicker</h1>
+        <button onClick={this.incrementor} >{this.state.timesClicked}</button>
+      </div>
+    )
+  }
+}
+
+export default DigitalClicker
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,58 @@
 // Code YouTubeDebugger Component Here
+import React, {Component} from 'react'
+
+class YouTubeDebugger extends Component {
+  state = {
+    errors: [],
+    user: null,
+    settings: {
+      bitrate: 8,
+      video: {
+        resolution: '1080p'
+      }
+    }
+  }
+
+// user spread operator
+  changeBit = () => {
+    const bitNum = this.state.settings.bitrate === 8 ? 12 : 8
+
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        bitrate: bitNum
+      }
+    })
+  }
+
+  changeRes = () => {
+    const res = this.state.settings.video.resolution === '1080p' ? '720p' : '1080p'
+
+    this.setState(js => ({
+      ...js,
+      settings: {
+        ...js.settings,
+        video: {
+          ...js.settings.video,
+            resolution: res
+        }
+      }
+    }))
+    
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>YouTube</h1>
+        <label>Bitrate</label>
+        <button className="bitrate" onClick={this.changeBit}>{this.state.settings.bitrate}</button>
+        <br/>
+        <label>Bitrate</label>
+        <button className="resolution" onClick={this.changeRes}>{this.state.settings.video.resolution}</button>
+      </div>
+    )
+  }
+}
+
+export default YouTubeDebugger
